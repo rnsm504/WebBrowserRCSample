f7abc7dbd28bc77fa1fde597d7e7d44116b15dc1
久世雅則 <masazak@gmail.com>
2016-06-20 00:23:57 +0900

    a

--- WebBrowserRCSample/ViewController.swift
+++ WebBrowserRCSample/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
     @IBOutlet weak var urlField: UITextField!
 //    @IBOutlet weak var myBrowser: UIWebView!
     
-    @IBOutlet weak var myLabel: UILabel!
     
     @IBOutlet weak var myWebView: UIWebView!
     
@@ -26,61 +25,55 @@ class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
     private var url : NSURL!
     private var result : MutableProperty<String> = MutableProperty("")
     
-    lazy var dynamicProperty: DynamicProperty! = DynamicProperty(object: self.myLabel, keyPath: "text")
-    
-    
     var cocoaAction : CocoaAction!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
-//        myBrowser.delegate = self
         
         self.urlField.delegate = self
         self.myWebView.delegate = self
         
-//        let a  = urlField.rac_textSignal()
-//            .toSignalProducer()
-//            .flatMapError{
-//                error in return SignalProducer<AnyObject?, NoError>.empty
-//            }
-//            .filter({ value -> Bool in
-//                return self.returnKey
-//            })
-//            .map {
-//                text in
-//                text as! String
-//            }
-//        vm.urlText <~ a
-        
-
-//        a.startWithNext { (results) in
-//            print("text is \(results)")
-//        }
-
-//        vm.action2.values.observeNext { value in
-//            print("next")
-//            self.myLabel.text = "aaa"
-//        }
-        
-//        vm.action2.events.observeNext { _ in
-//            print("aaa")
-//        }
-        
-        vm.action2.values.observeNext { value in
-            print("aaaa")
-            self.myLabel.text = "sasa"
+        vm.action3!.values.observeNext { value in
+            print(value.absoluteString)
+//            self.myLabel.text = "sasa"
             
             self.myWebView.loadRequest(NSURLRequest(URL: value))
         }
+        
+
+
+//                let a  = urlField.rac_textSignal()
+//                    .toSignalProducer()
+//                    .flatMapError{
+//                        error in return SignalProducer<AnyObject?, NoError>.empty
+//                    }
+////                    .filter({ value -> Bool in
+////                        return self.returnKey
+////                    })
+////                    .concat(SignalProducer<AnyObject?, NoError>)
+//                    .map {
+//                        text in
+//                        text as! String
+//                    }
+//                vm.urlText <~ a
+        
+        let a = self.urlField.rac_signalForControlEvents(.EditingDidEndOnExit)
+            .toSignalProducer()
+            .flatMapError{
+                error in return SignalProducer<AnyObject?, NoError>.empty
+            }
+            .map { value -> String in
+                 self.urlField.text!
+        }
 
         
-        cocoaAction = CocoaAction(vm.action2, input: self.urlField.text!)
-//        cocoaAction = CocoaAction(vm.action2, input: "")
-        
-//        urlField.addTarget(vm.action2.unsafeCocoaAction, action: CocoaAction.selector, forControlEvents: .EditingDidEndOnExit)
-  
+        vm.urlText <~ a
+        cocoaAction = CocoaAction(vm.action3, input: self.urlField.text!)
+////        cocoaAction = CocoaAction(vm.action2, input: "")
+//        
+////        urlField.addTarget(vm.action3, action: CocoaAction.selector, forControlEvents: .EditingDidEndOnExit)
+//  
         urlField.addTarget(cocoaAction, action: CocoaAction.selector, forControlEvents: .EditingDidEndOnExit)
         
     }
@@ -97,11 +90,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
         print("start")
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        
-//        textField.resignFirstResponder()
-        returnKey = true
-        return true
-    }
 }
 
